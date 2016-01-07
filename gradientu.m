39d685ac1b9fa71de96e1d8e41495cd2a4ca8516
Anna Harris <annaleeharris@gmail.com>
2016-01-07 12:51:25 -0600

    Create gradientu.m

---
+++ gradientu.m
@@ -0,0 +1,33 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function calculates gradient of the function u(x).
+% The reader may choose different model second order ellitpitc problems but
+% he/she has to match rhs.m, exactu.m, and gradientu.m function to the same
+% second order elliptic problems.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function gu=gradientu(node)
+
+x=node(:,1);
+y=node(:,2);
+
+% example 1
+%gux=(1-x).*y.*(1-y)-x.*y.*(1-y);  
+%guy=x.*(1-x).*(1-y)-x.*(1-x).*y;
+
+%example 2
+%gux = pi*cos(pi*y).*cos(pi*x);  
+%guy = -pi*sin(pi*x).*sin(pi*y);
+
+%example 3
+%gux = -.5*pi*sin(pi*(x+y));  
+%guy = -.5*pi*sin(pi*(x+y));
+
+%example 4
+%gux = (1-x).*(1-3*x);
+%guy = (1-y).*(1-3*y);
+
+%example 5
+gux = y.*(y - 1/sqrt(2)).*(2*x - 1/sqrt(2));
+guy = x.*(x - 1/sqrt(2)).*(2*y - 1/sqrt(2));
+
+gu=[gux,guy];
