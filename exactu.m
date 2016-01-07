025b7e7c546d3792b516bf2be7626d6a6dde7c62
Anna Harris <annaleeharris@gmail.com>
2016-01-07 12:48:02 -0600

    Create exactu.m

---
+++ exactu.m
@@ -0,0 +1,30 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function calculates gradient of the function u(x).
+% The reader may choose different model second order ellitpitc problems but
+% he/she has to match rhs.m, exactu.m, and gradientu.m function to the same
+% second order elliptic problems.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function u=exactu(node)
+
+x=node(:,1);
+y=node(:,2);
+
+%example 1
+%u=x.*(1-x).*y.*(1-y);  
+
+%example 2
+%u = sin(pi*x).*cos(pi*y);  %ex 2 from paper 1
+
+%example 3
+%u = .5*cos(pi*(x+y));
+
+%example 4
+%u = x.*(1-x).^2 + y.*(1-y).^2;
+
+%exaample 5
+u = x.*(x - 1/sqrt(2)).*y.*(y - 1/sqrt(2));
+
+
+
+
