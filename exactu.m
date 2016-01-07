0217a68cda99bf0f4d7f4720ee10ef983a1d48e1
Anna Harris <annaleeharris@gmail.com>
2016-01-07 12:49:30 -0600

    Update exactu.m

--- exactu.m
+++ exactu.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% This function calculates gradient of the function u(x).
+% This function calculates the exact solution of u.
 % The reader may choose different model second order ellitpitc problems but
-% he/she has to match rhs.m, exactu.m, and gradientu.m function to the same
+% one has to match rhs.m, exactu.m, and gradientu.m function to the same
 % second order elliptic problems.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
